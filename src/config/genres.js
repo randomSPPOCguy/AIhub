3c09f01160d88a6820cf084c15cf34e8942743d6
randomSPPOCguy <randomSPPOCguy@outlook.com>
2025-11-21 01:56:22 -0500

    Delete all existing files from project-frank branch

--- src/config/genres.js
+++
@@ -1,2 +0,0 @@
-import data from '../../config/genres.json' with { type: 'json' };
-export default data;
--- src/utils/logger.js
+++
@@ -1,6 +0,0 @@
-export const logger = {
-  info: console.log.bind(console, "[INF]"),
-  warn: console.warn.bind(console, "[WRN]"),
-  error: console.error.bind(console, "[ERR]"),
-  debug: (...args) => { if (process.env.LOG_LEVEL === "debug") console.debug("[DBG]", ...args); }
-};
